f2d125fd62b8ea40d2495105009f7bf1ad71a9fe
Ian <ianbrown9016@gmail.com>
2020-12-24 18:18:41 -0700

    put driver in separate file

---
+++ resumeFrontend.py
@@ -0,0 +1,14 @@
+import importlib
+import sys
+from PyQt5 import QtWidgets
+pm = __import__('resumeMainWindow')
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = pm.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+window()
